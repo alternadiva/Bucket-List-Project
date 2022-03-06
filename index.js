009e0f0537dda046d3d9f28385cc2002cb94ee6b
alternadiva <asomeonelse@gmail.com>
2022-03-06 17:53:12 +0000

    toggle category lists

--- index.js
+++ index.js
@@ -1,7 +1,10 @@
 let categoriesDiv = document.getElementById("categories-container");
 
+let n = 0;
+
+window.addEventListener("load", addDefaultCategories);
+
 function addDefaultCategories() {
-  let n = 0;
   while (n < 5) {
     let newCategory = document.createElement("div");
     newCategory.id = `category${n + 1}`;
@@ -11,6 +14,11 @@ function addDefaultCategories() {
     categoryHeader.className = "category-header";
     newCategory.appendChild(categoryHeader);
 
+    let listDiv = document.createElement("div");
+    listDiv.className = "list-div";
+    listDiv.id = `list${n + 1}`;
+    newCategory.appendChild(listDiv);
+
     categoryHeader.innerHTML = `
     <h3 class="category-title">Category ${n + 1}</h3>
     <p class="arrow">►</p>
@@ -18,8 +26,18 @@ function addDefaultCategories() {
 
     categoriesDiv.appendChild(newCategory);
 
+    categoriesDiv.childNodes.forEach((category) =>
+      category.addEventListener("click", toggleCategoryList)
+    );
+
     n++;
   }
 }
 
-window.addEventListener("load", addDefaultCategories);
+function toggleCategoryList() {
+  let listElement = this.childNodes[1];
+  listElement.classList.toggle("active");
+
+  let arrowElement = this.childNodes[0].getElementsByTagName("p")[0];
+  arrowElement.classList.toggle("rotate");
+}
