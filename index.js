fd264036eff5abc4f819aa164996c7880edeba6c
Hussain Jawad <45372216+HusJAW@users.noreply.github.com>
2022-03-09 18:46:42 +0000

    update

--- index.js
+++ index.js
@@ -115,7 +115,23 @@ setTimeout(() => {
   renderToDoListBtn();
 }, 10);
 
-function renderTodoItems() {}
+const addToCategoryBtn = document.querySelector(".add-category-btn");
+const modal = document.querySelector(".modal");
+const modalTitle = document.querySelector(".modal-title");
+const cancelBtn = document.querySelector(".cancel-btn");
+const addTodoBtn = document.querySelector(".submit-btn");
+const overlay = document.querySelector(".overlay");
+const modalInput = document.querySelector(".input");
+
+function modalToggle() {
+  modal.classList.toggle("hide");
+  overlay.classList.toggle("hide");
+}
+
+function modalCatgoryText() {
+  modalTitle.textContent = "add to category";
+  addTodoBtn.textContent = "Add Category";
+}
 
 window.addEventListener("click", (e) => {
   const target = e.target;
@@ -124,5 +140,12 @@ window.addEventListener("click", (e) => {
   if (target.classList.value === "btn") {
     let idValue = target.value;
     let listContainer = document.querySelector(`#list${idValue}`);
+    target.addEventListener("click", modalToggle);
+    cancelBtn.addEventListener("click", modalToggle);
+    target.addEventListener("click", modalCatgoryText);
   }
 });
+
+function renderTodoItems() {
+  const InputValue = document.querySelector(".input").value;
+}
